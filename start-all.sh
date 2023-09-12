01efc6cbd28a3cbad3ff0f8805512521a3e5fe21
Mario Calderon - Systemhaus Westfalia <mario.calderon@westfalia-it.com>
2023-09-12 09:05:46 -0600

    Add persistent directory to ZK container. It is mounted on same directory as Postgres container backup/database.

--- start-all.sh
+++ start-all.sh
@@ -2,24 +2,35 @@
 
 # Directory needed for storing persistently Postgres database.
 # Directory will be created only if inexistent.
-DBFILE=postgresql/postgres_database
-if [ ! -d "$DBFILE" ]; then
-    echo "Directory $DBFILE does not exist. It must be created."
-    echo "Create directory $DBFILE"
-    mkdir $DBFILE
+DBDIR=postgresql/postgres_database
+if [ ! -d "$DBDIR" ]; then
+    echo "Directory $DBDIR does not exist. It must be created."
+    echo "Create directory $DBDIR"
+    mkdir $DBDIR
 else
-    echo "Directory $DBFILE exists already: no need to create it"
+    echo "Directory $DBDIR exists already: no need to create it"
 fi
 
 # Backup directory for storing the Postgres backup file.
 # The name of the backup file must be "seed.backup"
-BACKUPFILE=postgresql/backups
-if [ ! -d "$BACKUPFILE" ]; then
-    echo "Directory $BACKUPFILE does not exist. It must be created."
-    echo "Create directory $BACKUPFILE"
-    mkdir $BACKUPFILE
+BACKUPDIR=postgresql/backups
+if [ ! -d "$BACKUPDIR" ]; then
+    echo "Directory $BACKUPDIR does not exist. It must be created."
+    echo "Create directory $BACKUPDIR"
+    mkdir $BACKUPDIR
 else
-    echo "Directory $BACKUPFILE exists already: no need to create it"
+    echo "Directory $BACKUPDIR exists already: no need to create it"
+fi
+
+# Directory needed for storing persistently files in ZK container.
+# Directory will be created only if inexistent.
+PERSISTENTDIR=postgresql/persistent_files
+if [ ! -d "$PERSISTENTDIR" ]; then
+    echo "Directory $PERSISTENTDIR does not exist. It must be created."
+    echo "Create directory $PERSISTENTDIR"
+    mkdir $PERSISTENTDIR
+else
+    echo "Directory $PERSISTENTDIR exists already: no need to create it"
 fi
 
 cp env_template .env
